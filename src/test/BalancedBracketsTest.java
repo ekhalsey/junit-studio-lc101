aa6160e8bc133a8a475b63766cfa53f9d0262a32
EmmaHalsey <ekhalsey@yahoo.com>
2022-08-31 20:27:48 -0500

    Class 5 studio Unit Testing

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,68 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
 
+    @Test
+    public void equalNumberOfOpenAndCloseBracketsReturnTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void oddNumberOfBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[]]]"));
+    }
+
+    @Test
+    public void firstCharacterClosingBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void ignoreNonBracketCharacters(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[LaunchCode]]"));
+    }
+
+    @Test
+    public void backwardsBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+    @Test
+    public void ignoresCurlyBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[{LaunchCode]"));
+        assertTrue(BalancedBrackets.hasBalancedBrackets("{{[[LaunchCode]]}}"));
+        assertTrue(BalancedBrackets.hasBalancedBrackets("{"));
+
+    }
+
+    @Test
+    public void ignoresNumbersInStrings() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[La123unchCode]]"));
+    }
+
+    @Test
+    public void stringHasNoBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("LaunchCode"));
+
+    }
+
+    @Test
+    public void ignoresSpecialCharacters() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[L@unch$%^&C*de]]"));
+    }
+
+    @Test
+    public void emptyStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void ignoresSpaces(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[Launch  Code]]"));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("L  aunchCode]]"));
+
+    }
 }
